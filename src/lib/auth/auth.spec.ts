91013058f39c7c09a40445b3e1aed0aea2e0a5a0
freesgen <jesusant.guerrero@gmail.com>
2020-11-03 20:02:20 -0400

    Add registration

--- src/lib/auth/auth.spec.ts
+++ src/lib/auth/auth.spec.ts
@@ -1,53 +1,97 @@
 /* eslint-disable functional/no-let */
 import test from 'ava';
+import sinon, { SinonSandbox } from 'sinon';
 
 import config from '../../config';
+import KanvasErrorAPI from '../shared/KanvasErrorAPI';
 
-import Auth  from './auth';
+import Auth from './auth';
 import TokenProvider from './tokenProvider';
 let auth: Auth;
+let sandbox: SinonSandbox;
 
 test.before(async () => {
-  auth = new Auth({
-    endpoint: config.apps.GEWAER_API,
-    appKey: config.appkey,
-  },
+  auth = new Auth(
+    {
+      endpoint: config.apps.GEWAER_API,
+      appKey: config.appkey,
+    },
     new TokenProvider({
-      domain: config.domain || null
+      domain: config.domain || null,
     })
   );
+
+  sandbox = sinon.createSandbox();
+});
+
+test.afterEach(() => {
+  sandbox.restore();
 });
 
 test('login success', async (t) => {
   auth.on('loggedIn', (token) => {
-    t.assert(token)
-  })
+    t.assert(token);
+  });
   const token = await auth.login(config.user, config.password);
   t.is(typeof token, 'string');
 });
 
 test('login fail: Email is not registered', async (t) => {
-  const error = await t.throwsAsync(auth.login("notemail@gmail.com", config.password))
-  t.is(error.message, "Request failed with status code 404")
+  const error = await t.throwsAsync<KanvasErrorAPI>(
+    auth.login('notemail@gmail.com', config.password)
+  );
+  t.is(error.message, 'Request failed with status code 404');
 });
 
 test('login fail: Wrong credentials', async (t) => {
-  const error = await t.throwsAsync(auth.login(config.user, "bad password"))
-  t.is(error.message, "Request failed with status code 404")
+  const error = await t.throwsAsync<KanvasErrorAPI>(
+    auth.login(config.user, 'bad password')
+  );
+  t.is(error.message, 'Request failed with status code 404');
 });
 
 test('login from cookies', async (t) => {
-  const token =  await auth.login(config.user, config.password);
+  const token = await auth.login(config.user, config.password);
   await auth.logout();
   auth.tokenProvider.setToken(token);
-  t.assert(auth.getToken())
+  t.assert(auth.getToken());
 });
-
 
 test('logout user', async (t) => {
   await auth.login(config.user, config.password);
   t.is(typeof auth.getToken(), 'string');
 
   await auth.logout();
-  t.falsy(auth.getToken())
+  t.falsy(auth.getToken());
+});
+
+test('registration fail: email taken', async (t) => {
+  const error = await t.throwsAsync<KanvasErrorAPI>(
+    auth.register({
+      lastname: 'Doe',
+      firstname: 'John',
+      email: config.user,
+      password: config.password,
+      verify_password: config.password,
+      default_company: 'John Corp',
+    })
+  );
+  t.is(error.errors.message, 'Users This email already has an account.');
+});
+
+test('registration success', async (t) => {
+  sandbox.stub(auth.http, 'request').resolves({
+    session: {
+      token: 'token',
+    },
+  });
+  const token = await auth.register({
+    lastname: 'Doe',
+    firstname: 'John',
+    email: 'test@example.com',
+    password: config.password,
+    verify_password: config.password,
+    default_company: 'John Corp',
+  });
+  t.truthy(token);
 });
---
+++ src/lib/shared/httpClient.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable functional/no-throw-statement */
+/* eslint-disable functional/immutable-data */
+/* eslint-disable functional/no-this-expression */
+/* eslint-disable functional/no-class */
+import axios, { AxiosInstance, AxiosRequestConfig} from "axios";
+
+import TokenProvider from "../auth/tokenProvider";
+
+import KanvasErrorAPI from "./KanvasErrorAPI";
+import ClientOptions from "./clientOptions";
+
+ export default class HttpClient {
+   readonly http: AxiosInstance;
+   readonly tokenProvider: TokenProvider
+
+   constructor(options: ClientOptions, tokenProvider: TokenProvider) {
+    this.http = axios.create({
+      baseURL: options.endpoint
+    });
+    this.tokenProvider = tokenProvider
+   }
+
+   async request(config: AxiosRequestConfig, isAuthorized = true): Promise<any|KanvasErrorAPI> {
+     if (!isAuthorized) {
+       const headers = {
+         Authorization: this.tokenProvider.getToken()
+       }
+       config.headers = Object.assign(config.headers, headers);
+     }
+     const { data } = await this.http.request(config).catch((error) => {
+       throw new KanvasErrorAPI(error);
+     })
+     return data;
+   }
+ }
